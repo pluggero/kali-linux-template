a39a46653d1dfb1e9c8330ccdb20c27ac988ce93
PluggeRo <robin@plugge-mail.de>
2025-08-14 01:57:05 +0200

    feat: Addition of post deploy custom

---
+++ scripts/kali_post_deploy_custom.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"/..
+
+source ./scripts/lib/shared.sh
+source ./scripts/lib/post_deploy_core.sh
+
+source ./scripts/config.sh
+# Optional override
+if [[ -n "${CONFIG_OVERRIDE:-}" && -f "$CONFIG_OVERRIDE" ]]; then
+  source "$CONFIG_OVERRIDE"
+fi
+POST_DEPLOY_PLAYBOOK="$ANSIBLE_DIR/playbooks/post-deploy-custom.yml"
+
+assert_dependencies
+
+wait_for_ssh "kali.local"
+
+clean_roles "$ROLES_DIR"
+ansible-galaxy install -r "$ANSIBLE_DIR/requirements.yml" --roles-path "$ROLES_DIR" --force
+
+VM_PASSWORD=$(extract_vm_password "$VAULT_FILE" "$VAULT_PASS_FILE")
+
+ANSIBLE_CONFIG="$ANSIBLE_CONFIG_FILE" \
+  ansible-playbook \
+  "$POST_DEPLOY_PLAYBOOK" \
+  --extra-vars ansible_become_password="$VM_PASSWORD"
